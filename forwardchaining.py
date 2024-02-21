e747f23f7dd8c9bc323da6405ae1e233013f4052
Srinivasa Pavan <82796461+srinivas50505@users.noreply.github.com>
2024-02-21 11:17:04 +0530

    Update forwardchaining.py

--- forwardchaining.py
+++ forwardchaining.py
@@ -1,29 +1,33 @@
-def forwardchain(rules,intial_Facts):
-    facts=intial_Facts.copy()
-    while True:
-        new_added_fact=False
-        for rule in rules:
-            result=evaluate(rule,facts)
-            if result and result not in facts:
-                facts.append(result)
-                new_added_fact=True
-        if not new_added_fact:
-                break
-    return facts
-def evaluate(rule,facts):
-    conditions,result =rule
-    if all(condition in facts for condition in conditions):
-        return result
-    return None
-        
-rules = [
-    (["fever", "cough"], "flu"),
-    (["fever", "sorethroat"], "flu"),
-    (["headache", "fatigue"], "migraine"),
-    (["runnynose"], "common cold"),
-    (["sneezing"], "allergies")
-]
+'''
+**INFERENCE RULES**
+mammal(A) ==> vertebrate(A).
+vertebrate(A) ==> animal(A).
+vertebrate(A),flying(A) ==> bird(A).
+vertebrate("duck").
+flying("duck").
+mammal("cat").
+'''
+global facts
+global is_changed
 
-initial_facts =  ["fever","cough", "throat"]
-conclusions = forwardchain(rules, initial_facts)
-print(" The Result is : ",conclusions[-1])
+is_changed = True
+facts = [["vertebrate","duck"],["flying","duck"],["mammal","cat"]]
+
+def assert_fact(fact):
+    global facts
+    global is_changed
+    if not fact in facts:
+        facts += [fact]
+        is_changed = True
+
+while is_changed:
+    is_changed = False
+    for A1 in facts:
+        if A1[0] == "mammal":
+            assert_fact(["vertebrate",A1[1]])
+        if A1[0] == "vertebrate":
+            assert_fact(["animal",A1[1]])
+        if A1[0] == "vertebrate" and ["flying",A1[1]] in facts:
+            assert_fact(["bird",A1[1]])
+
+print(facts)
